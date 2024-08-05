756e69978ed58a3c722e08478389b4ce90cb1603
FilipeBento21 <filipebentoheliodoro@gmail.com>
2024-08-05 01:00:05 -0300

    1º VERSÃO - SISTEMA BANCÁRIO;

--- BOOTCAMP DIO/SISTEMA_BANCÁRIO_COM_PYTHON.py
+++ BOOTCAMP DIO/SISTEMA_BANCÁRIO_COM_PYTHON.py
@@ -1,8 +1,10 @@
-#Começando com o Menu qye será exibido para o usuário:
+#Começando com o Menu qe será exibido para o usuário:
 
 menu = """
 
-Qual Operação Deseja Realizar ?
+      Olá Bem Vindo !!
+
+Qual Operação Deseja Realizar Hoje ?
 
 [1] Depositar;
 [2] Sacar;
@@ -10,7 +12,7 @@ Qual Operação Deseja Realizar ?
 [0] Sair;
 
 => """
-#exibindo menu e interagindo com o usuário;
+
 
 #Declarando Variáveis:
 
@@ -20,10 +22,15 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+#começando os códigos para as condições do sistema;
 
 while True:
 
+#exibindo menu e interagindo com o usuário;
+
     opcao = input(menu);
+
+#condições pos trás das opções oferecidas para o usuário no menu
 
     if opcao == "1":
         deposito = float(input("Informe O Quanto Deseja Depositar = "));
@@ -31,6 +38,11 @@ while True:
         if deposito > 0:
             saldo += deposito;
             extrato += f"Depósito Realizado ! Valor de Depósito: R${deposito:2.2f}\n"
+            print(f"""
+                  Deposito Realizado!
+                  
+                  Saldo Atual: R${saldo:1.2f}
+                  """);
 
         else:
             print("Operação falhou, favor Tentar novamente !");
@@ -38,29 +50,37 @@ while True:
     elif opcao == "2":
         saque = float(input("Informe O Valor Do Saque: "))
 
+#variáreis definidas com as operações necessárias para as condições da nossa 2º opção do while;
         excedeu_saldo = saque > saldo;
 
-        if excedeu_saldo:
-            print("Operação falhou, você não possui saldo o suficiente !")
-
-        excedeu_limite = saque > limite
-        
-        if excedeu_limite:
-            print("Operação falhou, Valor maior que o valor limite permitido !")
+        excedeu_limite = saque > limite;
 
         excedeu_saques = numero_saques >= LIMITE_SAQUES;
 
-        if excedeu_saques:
-            print("Operação falhou, número de saques máximo Atingido !")
-         
+        valor_invalido = saque < 0;
+        
+
         if saque > 0:
             saldo -= saque
             extrato += f"Saque realizado, valor sacado: R${saque:1.2f}\n"
             numero_saques += 1;
-        
-        valor_invalido = saque < 0
+            print(f"""\n=============================
+                  Saque Realizado !!
+                  
+                  Saldo Atual: R${saldo:1.2f}
+                  
+                  ===========================""");
 
-        if valor_invalido:
+        elif excedeu_saldo:
+            print("Operação falhou, você não possui saldo o suficiente !");
+
+        elif excedeu_limite:
+            print("Operação falhou, Valor maior que o valor limite permitido !");
+
+        elif excedeu_saques:
+            print("Operação falhou, número de saques máximo Atingido !")
+
+        else:         
             print("Operação falhou, o valor digitado é Invalido !")
 
     elif opcao == "3":
@@ -68,6 +88,7 @@ while True:
         print("Não foram Realizadas Movimentações" if not extrato else extrato)
         print(f"\n Saldo: R${saldo:1.2f}")
         print("==================================================")
+
     elif opcao == "0":
         break
 
