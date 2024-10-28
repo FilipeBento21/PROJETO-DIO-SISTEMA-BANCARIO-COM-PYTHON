2d539af535112ae36ca4ffaae7b61a319a91055c
FilipeBento21 <filipebentoheliodoro@gmail.com>
2024-10-28 01:07:10 -0300

    ADICIONANDO A FUNÇÃO DE LISTA DE  CONTAS
    Hoje finalizei a função nova conta, as ainda aparenta ter erros e irei revisar, também adicionei a listar contas, mas como ainda não está 100% a função criar contas, então estão as 2 esperando para teste

--- BOOTCAMP DIO/SISTEMA_BANCÁRIO_COM_PYTHON.py
+++ BOOTCAMP DIO/SISTEMA_BANCÁRIO_COM_PYTHON.py
@@ -2,59 +2,7 @@
 
 import textwrap;
 
-def main():
 
-#Declarando Variáveis Imutáveis:
-    LIMITE_SAQUES = 3;
-    AGENCIA = "0001";
-
-#Declarando Variáveis Mutáveis:
-    saldo = 0;
-    limite = 500;
-    extrato = "";
-    numero_saques = 0;
-    usuarios = [];
-    contas = [];
-    numero_conta = 0;
-    
-    
-    while True:
-        opcao = menu()
-
-        if opcao == "1":
-            valor = float(input("Informe o valor que deseja depositar: "));
-        
-            saldo, extrato = func_deposito(saldo, valor, extrato);
-
-        elif opcao == "2":
-            valor_de_saque = float(input("Informe o Valor do Saque: "));
-    
-            saldo, extrato = func_saque(
-                saldo=saldo,
-                valor_de_saque=valor_de_saque,
-                extrato=extrato,
-                limite=limite,
-                numero_saques=numero_saques,
-                limite_saques=LIMITE_SAQUES,
-            );
-        
-        elif opcao == "3":
-            extrato = func_extrato(saldo, extrato=extrato);
-
-        elif opcao == "4":
-            novo_usuario(usuarios);
-        
-        elif opcao == "5":
-            conta = nova_conta(AGENCIA, numero_conta, usuarios);
-
-            if conta:
-                contas.append(conta)
-
-        
-
-
-    else:
-        print("Operação Falhou, Favor tentar novamente !");
 
 def menu():
 
@@ -149,15 +97,85 @@ def filtrar_usuario(cpf,usuarios):
     usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
     return usuarios_filtrados[0] if usuarios_filtrados else None
 
-def nova_conta(AGENCIA, numero_conta, usuarios):
-    cpf = input("Informe seu CPF: ");
-    usuario = filtrar_usuario(cpf, usuarios);
+def nova_conta(agencia, numero_conta, usuarios, /):
+    cpf = input("Informe seu CPF: ")
+    usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
+
         print("\n Conta criada com sucesso !");
-        return {"agencia": AGENCIA, "número da conta": numero_conta, "usuário": usuario};
+        return {"agencia": agencia, "número_conta": numero_conta, "usuario": usuario}
 
     print("\n Usuário não encontado, Encerrando Processo de Criação;")
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta["agencia"]}
+            C/C\t\t{conta["numero_conta"]}
+            TItular:\t{conta["usuario"]['nome']}
+        """
+
+    print("=" * 100);
+    print(textwrap.dedent(linha))
+
+
+def main():
+
+#Declarando Variáveis Imutáveis:
+    LIMITE_SAQUES = 3;
+    AGENCIA = "0001";
+
+#Declarando Variáveis Mutáveis:
+    saldo = 0;
+    limite = 500;
+    extrato = "";
+    numero_saques = 0;
+    usuarios = [];
+    contas = [];
+    
+    
+    while True:
+        opcao = menu()
+
+        if opcao == "1":
+            valor = float(input("Informe o valor que deseja depositar: "));
+        
+            saldo, extrato = func_deposito(saldo, valor, extrato);
+
+        elif opcao == "2":
+            valor_de_saque = float(input("Informe o Valor do Saque: "));
+    
+            saldo, extrato = func_saque(
+                saldo=saldo,
+                valor_de_saque=valor_de_saque,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            );
+        
+        elif opcao == "3":
+            extrato = func_extrato(saldo, extrato=extrato);
+
+        elif opcao == "4":
+            novo_usuario(usuarios);
+        
+        elif opcao == "5":
+            numero_conta = len(contas) + 1;
+            conta = nova_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+
+        elif opcao == "6":
+            listar_contas(contas);
+
+        
+
+
+    else:
+        print("Operação Falhou, Favor tentar novamente !");
 
 
 main()
